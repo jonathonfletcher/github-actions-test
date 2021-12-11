721c21f8f5069aa0cba8fd60eefb630a874348e6
Jonathon Fletcher <jonathon.fletcher@gmail.com>
2021-12-10 16:54:09 -0800

    misc cleanup (#3)

--- example/main.go
+++ example/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"os"
 	"path/filepath"
@@ -13,11 +14,18 @@ var (
 )
 
 func main() {
-	//ctx := context.Background()
 	execName, _ := os.Executable()
+	versionMap := map[string]string{
+		"Version":   Version,
+		"BuildTime": BuildTime,
+		"VCSTag":    VCSTag,
+		"ExecName":  filepath.Base(execName),
+	}
+	versionString, _ := json.Marshal(versionMap)
+	log.Println(string(versionString))
 
-	log.Println("execName:", filepath.Base(execName))
 	log.Println("Version:", Version)
 	log.Println("BuildTime:", BuildTime)
 	log.Println("VCSTag:", VCSTag)
+	log.Println("ExecName:", filepath.Base(execName))
 }
